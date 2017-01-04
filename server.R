4ae565d6ba202fc23dd48327265b4eebaca4b59d
Ketki <ketkimuzumdar@hotmail.com>
2017-01-04 12:26:36 +0530

    Diamond App

--- server.R
+++ server.R
@@ -1,28 +1,78 @@
 library(shiny)
 library(ggplot2)
-shinyServer(function(input, output) {
+
+
+server <- function(input, output) {
   
-  model1 <- lm(price ~ carat, data = diamonds)
-  model2 <- lm(price ~ carat + cut, data = diamonds)
+  model1 <- lm(price ~ carat+cut+color+clarity, data = diamonds)
+
+  su<-summary(model1)
+  output$acc<-renderText({
+    
+    su$adj.r.squared * 100
+  })
   
   model1pred <- reactive({
     caratInput <- input$sliderCarat
-    predict(model1, newdata = data.frame(carat = caratInput))
-  })
-  
-  model2pred <- reactive({
-    caratInput <- input$sliderCarat
-    predict(model2, newdata = 
+    predict(model1, newdata = 
               data.frame(carat = caratInput,
-                         cut = input$dropDownCut))
+                         cut = input$dropDownCut,color=input$dropDownColor,
+                         clarity=input$dropDownClarity))
   })
-  
   
   output$pred1 <- renderText({
     model1pred()
   })
+  output$instructions <- renderUI(HTML("<ul><li><b>Price</b>: price in US dollars ($ 326-$ 18,823).</li>
+                                 <li><b>Carat</b>: weight of the diamond (0.2-5.01)</li>
+                                 <li><b>Cut</b>: quality of the cut (Fair, Good, Very Good, Premium, 
+Ideal)</li>
+                                 <li><b>Color</b>: diamond colour, from J (worst) to D (best)</li>
+                                 <li><b>Clarity</b>: a measurement of how clear the diamond is 
+                                 (I1 (worst), SI1, SI2, VS1, VS2, VVS1, VVS2, IF (best))</li>
+                                 </ul> 
+                                       <i>For more information go to documentation</i>"))
   
-  output$pred2 <- renderText({
-    model2pred()
+  
+  output$doc1<-renderUI(HTML("This app can be used to predict the price of diamonds with 
+respect to carats, cut, colour and clarity preferences. 
+The diamond dataset from ggplot2 package is used here to develope multiple regression model, 
+which predicts the price.
+
+The R file of the app can be studied <a href=https://github.com/ketki17489/Shiny-application->here</a>
+<br>
+
+<b>How to use app</b>
+
+<ol><li>select carats, cut, colour and clarity desired from left side pannel of the app.</li>
+<li>press Submit button.</li></ol>
+you will see the predicted price in main pannel.
+
+For more information Diamond dataset click <a href=http://docs.ggplot2.org/0.9.3.1/diamonds.html>here</a>
+                             "))
+  
+   
+   dataset <- reactive({
+    diamonds[sample(nrow(diamonds), input$sampleSize),]
   })
-})
+  
+    output$plot <- renderPlot({
+    
+    p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
+    
+    if (input$color != 'None')
+      p <- p + aes_string(color=input$color)
+    
+    facets <- paste(input$facet_row, '~', input$facet_col)
+    if (facets != '. ~ .')
+      p <- p + facet_grid(facets)
+    
+    if (input$jitter)
+      p <- p + geom_jitter()
+    if (input$smooth)
+      p <- p + geom_smooth()
+    
+    print(p)
+    
+  }, height=700)
+}
--- ui.R
+++ ui.R
@@ -1,20 +1,81 @@
 library(shiny)
+library(shinydashboard)
 library(ggplot2)
-shinyUI(fluidPage(
-  titlePanel("Predict price of diamond from carats and cut"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("sliderCarat", "What is the desired carat", 0.2000, 5.0100, value = 0.7000),
-      selectInput("dropDownCut", "Choose you Input:", choices = unique(diamonds$cut)),
+dataset <- diamonds
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Documentation", tabName = "doc", icon = icon("diamond")),
+      menuItem("Explore Diamond Dataset", tabName = "docEx", icon = icon("diamond"))
+    )
+  ),
+  dashboardBody(
+    
+    
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              h2("Diamond Price Prediction"),
+              fluidRow(
+                 box(
+                   title = "Controls",width = 6,
+                   sliderInput("sliderCarat", "Select Weight (Carat)", 0.2000, 5.0100, 
+                               value = 0.7000),
+                   selectInput("dropDownCut", "Choose Cut Quality:", 
+                               choices = unique(diamonds$cut)),
+                   selectInput("dropDownColor", "Choose Colour:", 
+                               choices = unique(diamonds$color)),
+                   selectInput("dropDownClarity", "Choose Clarity:", 
+                               choices = unique(diamonds$clarity)),
+                   submitButton("Submit")
+                 ),
+                 box(title = "Results",
+                   h3("Predicted Price for Diamond(US $):"),
+                   textOutput("pred1"),
+                   h3("Accuracy of Prediction(%):"),
+                   textOutput("acc")
+                 )
+              ),
+              fluidRow(
+                uiOutput("instructions")
+              )
+      ),
       
-      submitButton("Submit")
-    ),
-    mainPanel(
-      
-      h3("Predicted Price from Model 1:"),
-      textOutput("pred1"),
-      h3("Predicted Price from Model 2:"),
-      textOutput("pred2")
+      # Second tab content
+      tabItem(tabName = "doc",h2("About the App"),
+              uiOutput("doc1")
+      ),
+      # Third tab content
+      tabItem(tabName = "docEx",
+              h2("Explore Diamond Dataset"),
+              fluidRow(
+                       box(
+                         title = "Controls",width = 6,
+                         sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                                     value=min(1000, nrow(dataset)), step=500, round=0),
+                         
+                         selectInput('x', 'X', names(dataset)),
+                         selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+                         selectInput('color', 'Color', c('None', names(dataset))),
+                         
+                         checkboxInput('jitter', 'Jitter'),
+                         checkboxInput('smooth', 'Smooth'),
+                         
+                         selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+                         selectInput('facet_col', 'Facet Column', c(None='.', names(dataset))),
+                         submitButton("Submit")
+                       ),
+                       box(
+                         plotOutput('plot')
+                       )
+                )
+                
+                
+              )
+      )
     )
   )
-))
+  
